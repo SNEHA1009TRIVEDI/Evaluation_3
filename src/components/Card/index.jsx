1c55ff979aab6db935cf423580d21667022f2b4c
Sneha Trivedi <snehatrivedi1009@gmail.com>
2023-03-01 15:33:48 +0530

    Chore : modilfy the card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,58 +1,48 @@
 import React from "react";
 import "./Card.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { fas  } from "@fortawesome/free-solid-svg-icons";
-import { far  } from "@fortawesome/free-regular-svg-icons";
-import {
-  IconLookup,
-  IconDefinition,
-  findIconDefinition,
-  library,
-} from "@fortawesome/fontawesome-svg-core";
-library.add(fas);
-library.add(far);
+import { faCircleCheck, faBookmark } from "@fortawesome/free-solid-svg-icons";
+import makeRequest from "../../utils/makeRequest";
+
 const Card = (props) => {
   const { event } = props;
+  const [isBookmarked, setIsBookmark] = React.useState(event.isBookmarked);
+
   // console.log(event);
-  const circleCheckLookup: IconLookup = {
-    prefix: "fas",
-    iconName: "circle-check",
+  const bookmarkHandler = () => {
+    if (isBookmarked) {
+      makeRequest(
+        "PATCH",
+        `http://localhost:8000/api/events/${event.id}`,
+        false,
+        false
+      )
+        .then((res) => {
+          setIsBookmark(!isBookmarked);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      makeRequest(
+        "PATCH",
+        `http://localhost:8000/api/events/${event.id}`,
+        true,
+        true
+      )
+        .then((res) => {
+          setIsBookmark(!isBookmarked);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   };
-  const circleCheckIconDefinition: IconDefinition =
-    findIconDefinition(circleCheckLookup);
-  const circleXmarkLookup: IconLookup = {
-    prefix: "fas",
-    iconName: "circle-xmark",
-  };
-  const circleXmarkIconDefinition: IconDefinition =
-    findIconDefinition(circleXmarkLookup);
-  let lookup = circleCheckIconDefinition;
-  if (!event.areSeatsAvailable) {
-    lookup = circleXmarkIconDefinition;
-  }
-  const bookmarkLookup: IconLookup = {
-    prefix: "fas",
-    iconName: "bookmark",
-  };
-  const bookmarkIconDefinition: IconDefinition =
-    findIconDefinition(bookmarkLookup);
-  const bookmarkRegularLookup: IconLookup = {
-    prefix: "far",
-    iconName: "bookmark",
-  };
-  const bookmarkRegularIconDefinition: IconDefinition =
-    findIconDefinition(bookmarkRegularLookup);
-
-    let bookmark = bookmarkIconDefinition;
-  if(!event.isBookmarked){
-    bookmark = bookmarkRegularIconDefinition;
-  }
-
 
   return (
     <div className="card_container">
       <div className="card_image">
-        <img src={event.imgUrl} alt="event" className="image"/>
+        <img src={event.imgUrl} alt="event" className="image" />
       </div>
       <div className="card_content">
         <div className="card_title">
@@ -70,10 +60,13 @@ const Card = (props) => {
           </p>
         </div>
         <div className="card_footer">
-          {/* <img src={iconCheck} alt="" /> */}
-          <FontAwesomeIcon icon={lookup} />
-          <FontAwesomeIcon icon={bookmark} />
-          {/* <FontAwesomeIcon icon="fa-solid fa-circle-xmark" /> */}
+          <FontAwesomeIcon icon={faCircleCheck} />
+
+          <FontAwesomeIcon
+            icon={faBookmark}
+            className={isBookmarked ? "bookmarked" : "unmarked"}
+            onClick={bookmarkHandler}
+          />
         </div>
       </div>
     </div>
