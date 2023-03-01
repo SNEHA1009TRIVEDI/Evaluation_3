b18347c7c85dfb23a6425141032841647652d000
Sneha Trivedi <snehatrivedi1009@gmail.com>
2023-03-01 18:01:52 +0530

    Chore :create a change in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,42 @@
+// //create a header component
+// import React from "react";
+// import "./Footer.css";
+// const Footer = () => {
+//   return (
+//     <div className="Footer">
+//       <footer>Themes</footer>
+//     </div>
+//   );
+// };
+// export default Footer;
+
 //create a header component
 import React from "react";
+import Filter from "../Filter/index.jsx";
+import Card from "../Card/index.jsx";
+import makeRequest from "../../utils/makeRequest/index.js";
 import "./Footer.css";
+
 const Footer = () => {
+  const [theme, setTheme] = React.useState(null);
+
+  React.useEffect(() => {
+    makeRequest("GET", "http://localhost:8000/api/themes")
+      .then((res) => {
+        // console.log(res);
+        setTheme(res.data.themes);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+ console.log(theme);
+
   return (
-    <div className="Footer">
-      <footer>Themes</footer>
-    </div>
+    <>
+    <footer>Themes</footer>
+     
+    </>
   );
 };
 export default Footer;
